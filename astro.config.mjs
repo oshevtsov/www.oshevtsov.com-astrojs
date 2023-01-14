704987712773f36cccf370b97b064d3c31b7c321
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2023-01-14 10:34:23 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: "one-dark-pro",
+    },
+  },
+});
