5e4dd5e36bb90729dc3c5b2c5c2d0fbce582a4b0
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2023-01-16 15:03:40 +0100

    Reproduce old version of the web site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,22 @@
 import { defineConfig } from 'astro/config';
+import remarkUnwrapImages from 'remark-unwrap-images';
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import purgecss from "astro-purgecss";
+
+// https://astro.build/config
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    mdx(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [mdx(), purgecss(), compress()],
   markdown: {
+    remarkPlugins: [remarkUnwrapImages],
     shikiConfig: {
-      theme: "one-dark-pro",
-    },
-  },
+      theme: "one-dark-pro"
+    }
+  }
 });
