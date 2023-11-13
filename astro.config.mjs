67af9ef4d294fadd2adc36c8af8864156e097099
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2023-11-13 09:48:32 +0100

    Add blog post on using python with poetry and tox

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,9 @@ export default defineConfig({
   integrations: [mdx(), purgecss()],
   markdown: {
     remarkPlugins: [remarkUnwrapImages],
+    remarkRehype: {
+      footnoteLabel: "References",
+    },
     shikiConfig: {
       theme: "one-dark-pro",
     },
