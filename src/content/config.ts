2ee4da3b050f9f1a26ea35c84ed43dae3490ec3e
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2024-01-01 18:03:26 +0100

    Update website:
    - make og images dynamic
    - delegate image optimization to Astro
    - update CV

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,17 @@
 import { defineCollection, z } from "astro:content";
 
+// Note that custom 'slug' is not part of the schema, see
+// https://docs.astro.build/en/guides/content-collections/#defining-custom-slugs
 const blogCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    published: z.date(),
-    updated: z.date(),
-    abstract: z.string(),
-  }),
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      published: z.date(),
+      updated: z.date(),
+      abstract: z.string(),
+      cover: image().optional(),
+    }),
 });
 
 export const collections = {
